8df505fbe020d6d6a5e47d6c2df6f9049f0325b2
Rohitsengar02 <rohitsengar02@gmail.com>
2026-07-21 17:34:21 +0530

    all reload

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta httpEquiv="refresh" content="2" />
+      </head>
       <body>
         <AuthProvider>
           <CartProvider>
